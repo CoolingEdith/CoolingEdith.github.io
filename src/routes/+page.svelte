f32692af0bef9d992f351c41e6371089d2c71a66
Edith Lindström <edilin02@elevad.umea.se>
2023-10-18 15:43:17 +0200

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+	<h1>Min första hemsida</h1>+
+	<p>Vi erbjuder en kakmix för alla</p>
+	<br>
+	<p>Alla ska kunna baka!</p>
+
+	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEYaGaToc7mtXzm5ORlqP-ExDzuOMLUJVCHw&usqp=CAU">
+
+	<a target="_blank" href="https://www.instagram.com/mymixuf/">INSTAGRAM</a>
+
+
+    <style>
+        h1{
+            color:green;
+            font-size: 50px;
+        }
+
+        p {
+            font-size: xx-large;
+            font-weight: bolder;
+            color: rgba(154, 12, 12, 0.529);
+        }
+
+    </style>
