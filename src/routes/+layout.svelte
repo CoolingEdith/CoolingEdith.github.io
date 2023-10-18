1a95eda97897eb15d5ed1d04b591f22b125aff10
Edith Lindström <edilin02@elevad.umea.se>
2023-10-18 15:55:11 +0200

    my guy

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+
+
+<nav>
+    <a href="/">Hem</a>
+    <a href="/recept">Recept</a>
+    <a href="/info">Om oss</a>
+</nav>
+<slot></slot>
---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Min första hemsida</h1>+
+<p>Vi erbjuder en kakmix för alla</p>
+<br>
+<p>Alla ska kunna baka!</p>
