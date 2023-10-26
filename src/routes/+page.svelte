33eb9db677d24827384c0137fb7d3096db8289cb
Gustav Ydreborg <gusydr@elevad.umea.se>
2023-10-26 10:22:46 +0200

    enrre

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
-	<h1>Min första hemsida är fett!!!!!! coolaste!!!!!!</h1>
-	<h1>Min!!!!½!!! första hemsida är fett coolaste!!!!!!</h1>
+	<h1>Min första hemsida är fett coolaste</h1>
+	<h1>Min första hemsida är fett coolaste</h1>
 	<p>Vi erbjuder en kakmix för alla</p>
 	<br>
 	<p>Alla ska kunna baka!</p>
+    <br>
 
     
 
-
+   
 	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEYaGaToc7mtXzm5ORlqP-ExDzuOMLUJVCHw&usqp=CAU">
 
 	<a target="_blank" href="https://www.instagram.com/mymixuf/">Våran instagram</a>
@@ -21,7 +22,7 @@
         p {
             font-size: xx-large;
             font-weight: bolder;
-            color: rgba(83, 25, 198, 0.697);
+            color: rgba(83, 25, 198, 0.666);
         }
 
     </style>
