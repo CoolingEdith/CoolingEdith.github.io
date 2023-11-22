c46d84a946bc647a7dc5d50c49414a275197e3dd
Gustav Ydreborg <gusydr@elevad.umea.se>
2023-11-22 14:50:47 +0100

    me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,7 @@
 <br>
 <h1>Såhär gör du:</h1><br>
 
-<img src="UF-smör.png" alt="">
-<img src="UF-mixen.png" alt="">
-<img src="UF-vatten.png" alt="">
-<img src="UF-smör.png" alt="">
-<img src="UF-smör.png" alt="">
-<img src="UF-smör.png" alt="">
-<img src="UF-smör.png" alt="">
+
 
 
 <p>Vi erbjuder en kakmix för alla</p>
