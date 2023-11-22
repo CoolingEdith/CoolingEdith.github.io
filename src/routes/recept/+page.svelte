8c629dd7b7e3a072e86d4b506da346b1818254cc
Gustav Ydreborg <gusydr@elevad.umea.se>
2023-11-22 12:45:15 +0100

    mememe

--- src/routes/recept/+page.svelte
+++ src/routes/recept/+page.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
   const images = [
     [
-      "UF-mixen.png",
+      "UF-ugn2.png",
+      "UF-rörihop.png",
       "UF-kyl.png",
+    ],
+    [
+      "UF-smör.png",
+      "UF-vatten.png",
       "UF-plåt.png",
     ],
     [
-      "UF-rörihop.png",
+      "UF-mixen.png",
       "UF-rulla.png",
-      "UF-smör.png",
-    ],
-    [
-      "UF-ugn.png",
-      "UF-ugn2.png",
-      "UF-vatten.png",
-    ],
-    [
-      "https://images.unsplash.com/photo-1633053663400-655b31fb88ac?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4MDV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-      "https://images.unsplash.com/photo-1578170222009-c7893aa20afd?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4MTZ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-      "https://images.unsplash.com/photo-1610220941077-1ec123e7c043?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY4NjV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
+      "UF-ugn.png",,
     ],
   ];
 </script>
