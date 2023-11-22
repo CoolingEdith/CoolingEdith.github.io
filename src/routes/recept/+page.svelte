647ee3ae7bd6b93ef164a7452e9b48d3d2a7d5d9
Gustav Ydreborg <gusydr@elevad.umea.se>
2023-11-22 12:34:00 +0100

    2 bilder

--- src/routes/recept/+page.svelte
+++ src/routes/recept/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   const images = [
     [
-      "https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
+      "UF-kyl.png",
       "UF-mixen.png",
-      "https://images.unsplash.com/photo-1591775161903-497839a443c1?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3MzR8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-
+      "UF-plåt.png",
     ],
     [
       "https://images.unsplash.com/photo-1617296539691-67feaadf389e?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3NjF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
