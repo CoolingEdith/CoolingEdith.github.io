a9cd5011e293729335c4880f4601a7274846a867
Edith Lindström <edilin02@elevad.umea.se>
2023-11-23 11:01:52 +0100

    hk

--- README.md
+++ README.md
@@ -49,3 +49,24 @@ QR-kod
 
 
 länk in till vår webbsida: https://mymixuf.github.io/
+
+<!--<br>
+<h1>Såhär gör du:</h1><br>
+
+
+
+
+<p>Vi erbjuder en kakmix för alla</p> 
+  
+
+
+
+<style>
+
+  p {
+    font-size: 20px;
+    font-weight: bolder;
+    color: rgb(255, 255, 255);
+  }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,51 @@
-
 <br>
 <h1>Såhär gör du:</h1><br>
 
+<script lang="ts">
+  const images = [
+    [
+      "UF-smör.png",
+      "UF-mixen.png",
+      "UF-vatten.png",
+      "UF-blanda.png",
+      "UF-kyl.png",
+      "UF-ugn2.png",
+      "UF-rulla.png",
+      "UF-plåt.png",
+      "UF-ugn.png",
+    ]
+  ]
+</script>
 
-
-
-<p>Vi erbjuder en kakmix för alla</p> 
-  
-
-
+<section class="p-10 grid grid-cols-2 md:grid-cols-4 gap-2">
+  {#each images as imageGroup}
+    <div class="grid gap-40">
+      {#each imageGroup as imgUrl}
+        <div>
+          <img class="h-auto max-w-full rounded-lg" src={imgUrl} alt="" />
+        </div>
+      {/each}
+    </div>
+  {/each}
+</section>
 
 <style>
+  img {
+    transition: all 0.2s;
+    transform: scale(1.2);
 
-  p {
-    font-size: 20px;
-    font-weight: bolder;
-    color: rgb(255, 255, 255);
   }
+
+  img:hover {
+    transform: scale(1.23);
+  }
+
+
+
+  h1 {
+    color: rgb(0, 174, 254);
+    font-size: xx-large;
+  }
+  
 
 </style>
